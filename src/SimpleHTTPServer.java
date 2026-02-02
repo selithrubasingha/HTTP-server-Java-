1a05df63b8845310996c5dedeca3d54bdf422b4c
Selith <selithrubasingha@gmail.com>
2026-02-02 20:14:44 +0530

    trying to send output to the browser

--- src/SimpleHTTPServer.java
+++ src/SimpleHTTPServer.java
@@ -3,6 +3,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 
 public class SimpleHTTPServer {
@@ -11,14 +13,23 @@ public class SimpleHTTPServer {
         final ServerSocket server = new ServerSocket(8080);
         System.out.println("Listening for connection on port 8080 ....");
         while (true) {
-            final Socket client = server.accept();
-            InputStreamReader isr = new InputStreamReader(client.getInputStream());
-            BufferedReader reader = new BufferedReader(isr);
-            String line = reader.readLine();
+//            final Socket socket = server.accept();
+//            InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+//            BufferedReader reader = new BufferedReader(isr);
+//            String line = reader.readLine();
+//
+//            if (!line.isEmpty()){
+//                System.out.println(line);
+//                line = reader.readLine();
+//            }
 
-            if (!line.isEmpty()){
-                System.out.println(line);
-                line = reader.readLine();
+            //Giving out to the HTTP server
+            try (Socket socket = server.accept()){
+                Date today = new Date();
+                String HTTPResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+
+                socket.getOutputStream().write(HTTPResponse.getBytes("UTF-8"));
+
             }
 
         }
