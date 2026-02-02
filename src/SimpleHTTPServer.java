246785604d0c9c82016d71f732702708620b7c48
Selith <selithrubasingha@gmail.com>
2026-02-02 19:36:07 +0530

    reading a GET HTTP request

--- src/SimpleHTTPServer.java
+++ src/SimpleHTTPServer.java
@@ -1,3 +1,6 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -9,10 +12,15 @@ public class SimpleHTTPServer {
         System.out.println("Listening for connection on port 8080 ....");
         while (true) {
             final Socket client = server.accept();
-            // 1. Read HTTP request from the client socket
-            // 2. Prepare an HTTP response
-            // 3. Send HTTP response to the client
-            // 4. Close the socket
+            InputStreamReader isr = new InputStreamReader(client.getInputStream());
+            BufferedReader reader = new BufferedReader(isr);
+            String line = reader.readLine();
+
+            if (!line.isEmpty()){
+                System.out.println(line);
+                line = reader.readLine();
+            }
+
         }
 
     }
