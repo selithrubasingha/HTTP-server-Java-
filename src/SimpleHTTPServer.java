ddf4395b4bd063068787ca643b1fe466f4d91b60
Selith <selithrubasingha@gmail.com>
2026-02-02 19:27:38 +0530

    adding socket logic

--- src/SimpleHTTPServer.java
+++ src/SimpleHTTPServer.java
@@ -1,4 +1,5 @@
 import java.net.ServerSocket;
+import java.net.Socket;
 
 
 public class SimpleHTTPServer {
@@ -7,7 +8,11 @@ public class SimpleHTTPServer {
         final ServerSocket server = new ServerSocket(8080);
         System.out.println("Listening for connection on port 8080 ....");
         while (true) {
-
+            final Socket client = server.accept();
+            // 1. Read HTTP request from the client socket
+            // 2. Prepare an HTTP response
+            // 3. Send HTTP response to the client
+            // 4. Close the socket
         }
 
     }
